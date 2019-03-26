2a8efdac485e4831e7da7720b7af3eb48bb7efe5
泡海椒 <814780613@qq.com>
2019-03-26 22:48:24 +0800

    stack

--- src/main/java/com/spark/service/SparrkSql.scala
+++ src/main/java/com/spark/service/SparrkSql.scala
@@ -7,6 +7,8 @@ import com.spark.bean.TransferInfo
 import org.apache.spark.sql.types._
 import org.apache.spark.sql.{SQLContext, SaveMode}
 import org.apache.spark.{SparkConf, SparkContext}
+
+import scala.collection.JavaConverters
 case class itf_grey_list(id:Int,ent_name:String,grey_type:String,describe:String,create_by:String,date_created:Timestamp,grey_flag:String)
 
 object SparrkSql {
@@ -27,11 +29,13 @@ object SparrkSql {
     val prop = new Properties()
     prop.setProperty("user",tansfer.getSourceUser)
     prop.setProperty("password",tansfer.getSourcePassw)
-    var jdbcDF = sqlContext.read.jdbc(tansfer.getSourceJdbc,tansfer.getSourceSchema+"."+tansfer.getSourceTable,prop)
     import sqlContext.implicits._
-    var rows=jdbcDF.collect().map(e=>itf_grey_list(e.getInt(0)+50,e.getString(1),e.getString(2),e.getString(3),e.getString(4),e.getTimestamp(5),e.getString(6)))
-    val jdbcDFTo=sc.parallelize(rows,2).toDF()
-    import sqlContext.implicits._
+    var rows = sqlContext.read.jdbc(tansfer.getSourceJdbc, tansfer.getSourceSchema + "." + tansfer.getSourceTable, prop).collectAsList();
+    val seq = JavaConverters.asScalaIteratorConverter(rows.iterator).asScala.toSeq
+    val jdbcDFTo = sc.parallelize(seq, 2)
+    val column = ""
+    val structType = buildStructType(column)
+    val jdbcDF = sqlContext.createDataFrame(jdbcDFTo, structType)
     //jdbcDF.
     Class.forName(tansfer.getToClass);
     val prop1 = new Properties()
@@ -46,9 +50,27 @@ object SparrkSql {
      // jdbcDF.drop(jdbcDF(id"))
     }
 
-    jdbcDFTo.write.mode(SaveMode.Overwrite).jdbc(tansfer.getToJdbc,tansfer.getToSchema+"."+tansfer.getToTable,prop1);
+    jdbcDF.write.mode(SaveMode.Overwrite).jdbc(tansfer.getToJdbc, tansfer.getToSchema + "." + tansfer.getToTable, prop1);
     sc.stop();
 
+  }
+
+  def buildStructType(fields: String): StructType = {
+    StructType(fields.split(",").map(_.split(":"))
+      .map(field => {
+        val fieldType = field(1).toString.toLowerCase() match {
+          case "string" => StringType
+          case "int" => IntegerType
+          case "integer" => IntegerType
+          case "double" => DoubleType
+          case "float" => FloatType
+          case "long" => LongType
+          case "boolean" => BooleanType
+          case "Date" => DateType
+          case _ => StringType
+        }
+        StructField(field(0), fieldType, true)
+      }))
   }
 
 }
